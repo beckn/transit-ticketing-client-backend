90d6075f419db791e98c88b20ff346a6aabfa792
vidhiagar <vidhi.ami@gmail.com>
2021-11-26 20:38:14 +0530

    Changes for Station API query and Data
    Changes for Station API query and Data

--- src/main/java/com/transit/ticketing/service/StationService.java
+++ src/main/java/com/transit/ticketing/service/StationService.java
@@ -1,15 +1,13 @@
 package com.transit.ticketing.service;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 
 import com.transit.ticketing.entity.Station;
 
-import java.util.List;
+public interface StationService {
 
-public interface StationService{
-
-	public ResponseEntity<List<Station>>  getStationsById(String stationId);
-
-	public ResponseEntity<List<Station>>  getStationsByLocation(String lat, String lon);
+	public ResponseEntity<List<Station>> getStationsById(String stationId);
 
 }
