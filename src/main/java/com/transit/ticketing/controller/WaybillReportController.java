57b5f25ed3231ef763a801d9043654cb022ecd3a
sagar <sagarinpursue@gmail.com>
2022-05-13 02:33:52 +0530

    Updating the waybill controller to respond back from the db.

--- src/main/java/com/transit/ticketing/controller/WaybillReportController.java
+++ src/main/java/com/transit/ticketing/controller/WaybillReportController.java
@@ -1,6 +1,10 @@
 package com.transit.ticketing.controller;
 
+import com.transit.ticketing.dto.WaybillReportDto;
+import com.transit.ticketing.entity.Routes;
 import com.transit.ticketing.entity.Staff;
+import com.transit.ticketing.entity.Trips;
+import com.transit.ticketing.repository.WaybillReportRepository;
 import com.transit.ticketing.service.WaybillReportService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +14,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class WaybillReportController {
@@ -19,9 +26,21 @@ public class WaybillReportController {
     @Autowired
     private WaybillReportService waybillReportService;
 
+    @Autowired
+    private WaybillReportRepository waybillReportRepository;
+
     @GetMapping(value="/api/v1/secure/generateWayBillReport")
     public ResponseEntity<Object> filterStaffBasedOnRole() {
         return ResponseEntity.ok().body(waybillReportService.updateWaybillReportData());
+    }
+
+    @GetMapping(value="/api/v1/secure/waybillreport")
+    public ResponseEntity<Object> getWaybillReport() {
+        Map<String, Object> report = new HashMap<>();
+        report.put("waybillReports", waybillReportRepository.findAll());
+        report.put("waybillReportsCount", waybillReportRepository.count());
+
+        return ResponseEntity.ok().body(report);
     }
 
 }
